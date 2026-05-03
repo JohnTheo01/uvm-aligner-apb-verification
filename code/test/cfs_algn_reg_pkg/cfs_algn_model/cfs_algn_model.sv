0adfb14032dc8960d76dd24c38ff179ddec040f8
johntheo <giannis.theodorakis2001@gmail.com>
2026-05-03 19:30:06 +0300

    update: implement reset handler in register model

--- code/test/cfs_algn_reg_pkg/cfs_algn_model/cfs_algn_model.sv
+++ code/test/cfs_algn_reg_pkg/cfs_algn_model/cfs_algn_model.sv
@@ -2,12 +2,16 @@
 
     `define CFS_ALGN_MODEL_SV
 
-    class cfs_algn_model extends uvm_component;
+    class cfs_algn_model 
+        extends uvm_component
+        implements uvm_ext_reset_handler;
 
         `uvm_component_utils(cfs_algn_model)
 
         // Register Model
         cfs_algn_reg_block reg_block;
+
+        cfs_algn_env_config env_config;
 
         function new(string name = "", uvm_component parent);
             super.new(name, parent);
@@ -22,6 +26,16 @@
                 reg_block.build();
                 reg_block.lock_model();
             end
+        endfunction
+
+        virtual function void end_of_elaboration_phase(uvm_phase phase);
+            super.end_of_elaboration_phase(phase);
+
+            reg_block.CTRL.set_algn_data_width(env_config.get_algn_data_width());
+        endfunction
+
+        virtual function void handle_reset(uvm_phase phase);
+            reg_block.reset("HARD");
         endfunction
 
     endclass
