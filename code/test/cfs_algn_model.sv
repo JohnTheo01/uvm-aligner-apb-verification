8a589cfe0148177faff90345a39756711ad40daf
johntheo <giannis.theodorakis2001@gmail.com>
2026-05-04 20:51:44 +0300

    feat: add analysis ports to model and connect MD monitor outputs in env
    Added input analysis ports (port_in_rx, port_in_tx) to receive data from
    the MD RX/TX monitors, output ports (port_out_rx, port_out_tx) for the
    scoreboard, and port_out_irq for the IRQ signal. Connected the MD monitor
    output ports to the model in the environment connect_phase.

--- code/test/cfs_algn_model.sv
+++ code/test/cfs_algn_model.sv
@@ -2,6 +2,9 @@
 
     `define CFS_ALGN_MODEL_SV
 
+    `uvm_analysis_imp_decl(_in_rx)
+    `uvm_analysis_imp_decl(_in_tx)
+
     class cfs_algn_model 
         extends uvm_component
         implements uvm_ext_reset_handler;
@@ -11,10 +14,30 @@
         // Register Model
         cfs_algn_reg_block reg_block;
 
+        // Env_config handler
         cfs_algn_env_config env_config;
+
+        // Input ports handlers
+        uvm_analysis_imp_in_rx#(cfs_md_item_mon, cfs_algn_model) port_in_rx;
+        uvm_analysis_imp_in_tx#(cfs_md_item_mon, cfs_algn_model) port_in_tx;
+
+        // Output ports to scoreboard
+        uvm_analysis_port#(cfs_md_response) port_out_rx;
+        uvm_analysis_port#(cfs_md_item_mon) port_out_tx;
+
+        // Intertupt request port
+        uvm_analysis_port#(bit) port_out_irq;
 
         function new(string name = "", uvm_component parent);
             super.new(name, parent);
+
+            port_in_rx = new("port_in_rx", this);
+            port_in_tx = new("port_in_tx", this);
+
+            port_out_rx = new("port_out_rx", this);
+            port_out_tx = new("port_out_tx", this);
+
+            port_out_irq = new("port_out_irq", this);
         endfunction
 
         virtual function void build_phase(uvm_phase phase);
@@ -51,6 +74,24 @@
 
         virtual function void handle_reset(uvm_phase phase);
             reg_block.reset("HARD");
+        endfunction
+
+        virtual function void write_in_rx(cfs_md_item_mon item_mon);
+            `uvm_info("DEBUG", $sformatf(
+                "Model received information from the RX agent: %0s", 
+                item_mon.convert2string()
+                ),
+                UVM_NONE
+            )
+        endfunction
+
+        virtual function void write_in_tx(cfs_md_item_mon item_mon);
+            `uvm_info("DEBUG", $sformatf(
+                "Model received information from the TX agent: %0s", 
+                item_mon.convert2string()
+                ),
+                UVM_NONE
+            )
         endfunction
 
     endclass
