1bb52a6aea8f2bcf64c2fce7a472ab157bcf7acd
johntheo <giannis.theodorakis2001@gmail.com>
2026-05-08 19:41:18 +0300

    fix: add exp_rx_response_threshold to env_config and remove dead code from model
    - cfs_algn_env_config: added exp_rx_response_threshold field (default 10) with
      getter/setter, needed by the scoreboard watchdog; reorganized field declarations
      and phase method ordering
    - cfs_algn_model: removed commented-out port_out_rx.write() and minor whitespace cleanup

--- code/test/cfs_algn_env_config.sv
+++ code/test/cfs_algn_env_config.sv
@@ -5,11 +5,13 @@
     class cfs_algn_env_config
         extends uvm_component;
 
+        protected cfs_algn_vif vif;
+
         local bit has_checks;
 
         local int unsigned algn_data_width;
 
-        protected cfs_algn_vif vif;
+        local int unsigned exp_rx_response_threshold;
 
         `uvm_component_utils(cfs_algn_env_config)
 
@@ -18,9 +20,23 @@
 
             has_checks = 1;
             algn_data_width = 8;
+            exp_rx_response_threshold = 10;
         endfunction
 
+        virtual function void start_of_simulation_phase(uvm_phase phase);
+            super.start_of_simulation_phase(phase);
+        
+            if (this.vif == null) begin
+                `uvm_fatal("ALGORITHM_ISSUE", "Virtual Interface has not been instantiated in \"start_of_simulation_phase\" phase")
+            end else begin
+                `uvm_info("DEBUG", "Virtual Interface has been instantiated in \"start_of_simulation_phase\" phase", UVM_DEBUG)
+            end
 
+        endfunction
+
+        // ----------------------------------- Setters - Getters -----------------------------------
+
+        // ----------------------------------- has_checks -----------------------------------
         virtual function bit get_has_checks();
             return this.has_checks;
         endfunction
@@ -29,6 +45,7 @@
             this.has_checks = value;
         endfunction
 
+        // ----------------------------------- align_data_width -----------------------------------
         virtual function int unsigned get_algn_data_width();
             return this.algn_data_width;
         endfunction
@@ -37,10 +54,10 @@
             if ((value < 8) || ($countones(value) != 1)) begin
                 `uvm_fatal("ALGORITHM_ISSUE", "You need to specify correct values for \"algn_data_width\"")
             end 
-            
             this.algn_data_width = value;
         endfunction
 
+        // ----------------------------------- vif -----------------------------------
         virtual function void set_vif(cfs_algn_vif value);
             if (this.vif == null) begin
                 this.vif = value;
@@ -48,22 +65,20 @@
             end
 
             `uvm_fatal("ALGORITHM_ISSUE", "Trying to set the virtual interface more than once")
-
         endfunction
 
         virtual function cfs_algn_vif get_vif();
             return this.vif;
         endfunction
 
-        virtual function void start_of_simulation_phase(uvm_phase phase);
-            super.start_of_simulation_phase(phase);
-            
-            if (this.vif == null) begin
-                `uvm_fatal("ALGORITHM_ISSUE", "Virtual Interface has not been instantiated in \"start_of_simulation_phase\" phase")
-            end else begin
-                `uvm_info("DEBUG", "Virtual Interface has been instantiated in \"start_of_simulation_phase\" phase", UVM_DEBUG)
-            end
+      
+        // ----------------------------------- exp_rx_respnse_threshold -----------------------------------
+        virtual function void set_exp_rx_respnse_threshold(int unsigned value);
+            this.exp_rx_response_threshold = value;
+        endfunction
 
+        virtual function int unsigned get_exp_rx_respnse_threshold();
+            return this.exp_rx_response_threshold;
         endfunction
 
     endclass
