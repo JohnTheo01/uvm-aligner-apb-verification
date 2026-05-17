f35d95143bf7ba97fba4419699b8d038e293075c
PC work <giannis.theodorakis2001@gmail.com>
2026-05-17 14:18:10 +0300

    test: switch random test to virtual sequencer
    - Replace direct md_rx sequence with cfs_algn_virtual_sequence_slow_pace
      on the virtual sequencer
    - Add 5 APB transactions to exercise the JSON recorder
    - Virtual sequence drives 8 MD transactions
    - Constrain CTRL register: SIZE=2, OFFSET=0

--- code/test/cfs_algn_test/cfs_algn_test_random.sv
+++ code/test/cfs_algn_test/cfs_algn_test_random.sv
@@ -15,41 +15,58 @@
             uvm_status_e status;
     
             phase.raise_objection(this, "TEST_DONE");
-
-            `uvm_info("DEBUG", "start of test", UVM_LOW)
-
         
             #(100ns);
 
-            fork
-                begin
-                    cfs_md_sequence_slave_response_forever seq_response_forever = 
-                        cfs_md_sequence_slave_response_forever::type_id::create("seq_response_forever");
+            // fork
+            //     begin
+            //         cfs_md_sequence_slave_response_forever seq_response_forever = 
+            //             cfs_md_sequence_slave_response_forever::type_id::create("seq_response_forever");
                     
-                    seq_response_forever.start(env.md_tx_agent.sequencer);
-                end
-            join_none
+            //         seq_response_forever.start(env.md_tx_agent.sequencer);
+            //     end
+            // join_none
 
             env.model.reg_block.IRQEN.write(status, 5'b11111);
 
+             // 5 APB transactions για testing του recorder
+            repeat(5) begin
+                cfs_apb_sequence_simple seq;
+                seq = cfs_apb_sequence_simple::type_id::create("seq");
+                void'(seq.randomize());
+                seq.start(env.apb_agent.sequencer);
+            end
 
-            void'(env.model.reg_block.CTRL.randomize());
+            void'(env.model.reg_block.CTRL.randomize() with{
+                SIZE.value == 2;
+                OFFSET.value == 0;
+            });
             env.model.reg_block.CTRL.update(status);
 
+           
+
           
-            repeat(100) begin 
-                cfs_md_sequence_simple_master seq_simple = cfs_md_sequence_simple_master::type_id::create("seq_simple");
+            repeat(1) begin 
+                // cfs_md_sequence_simple_master seq_simple = cfs_md_sequence_simple_master::type_id::create("seq_simple");
 
-                seq_simple.set_sequencer(env.md_rx_agent.sequencer);
+                // seq_simple.set_sequencer(env.md_rx_agent.sequencer);
                 
-                void'(seq_simple.randomize());
+                // void'(seq_simple.randomize());
 
-                seq_simple.start(env.md_rx_agent.sequencer);
+                // seq_simple.start(env.md_rx_agent.sequencer);
+
+                cfs_algn_virtual_sequence_slow_pace vseq_slow_pace;
+                vseq_slow_pace = cfs_algn_virtual_sequence_slow_pace::type_id::create("vseq_slow_pace");
+
+                vseq_slow_pace.set_sequencer(env.virtual_sequencer);
+
+                void'(vseq_slow_pace.randomize());
+
+                vseq_slow_pace.start(env.virtual_sequencer);
+
             end
             
             #(500ns);
-
-            `uvm_info("DEBUG", "end of test", UVM_LOW)
 
             phase.drop_objection(this, "TEST_DONE");
 
