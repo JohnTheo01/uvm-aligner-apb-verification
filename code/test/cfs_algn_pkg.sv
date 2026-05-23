1185103b031004084a5873561795c8e36d15ea48
PC work <giannis.theodorakis2001@gmail.com>
2026-05-23 15:28:35 +0300

    feat: add reg_config, reg_status and rx virtual sequences
    - reg_config: writes random values to all RW/WO registers in shuffled order
    - reg_status: reads all RO registers (currently STATUS)
    - rx: drives a single randomized MD RX transaction via virtual sequencer

--- code/test/cfs_algn_pkg.sv
+++ code/test/cfs_algn_pkg.sv
@@ -44,6 +44,10 @@
     `include "cfs_algn_virtual_sequence_slow_pace.sv"
     `include "cfs_algn_virtual_sequence_reg_access_random.sv"
     `include "cfs_algn_virtual_sequence_reg_access_unmapped.sv"
+    
+    `include "cfs_algn_virtual_sequence_reg_config.sv"
+    `include "cfs_algn_virtual_sequence_reg_status.sv"
+    `include "cfs_algn_virtual_sequence_rx.sv"
 
     `include "cfs_algn_env.sv"
 
